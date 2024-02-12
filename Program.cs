85b730dd9f516cdb35fe4d8869ab4004d1174d44
furia <github@desatatufuria.com>
2024-02-12 16:57:05 +0100

    Nueva Version

--- Program.cs
+++ Program.cs
@@ -1,99 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CifradoCesar
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            //Console.WriteLine("Mensaje Recibido:");
-            //string mensaje = Console.ReadLine();
+        string mensajeOriginal = "bkbw";
+        int clave = 2;
 
+        string mensajeDescifrado = desencriptarCesar(mensajeOriginal, clave);
+        Console.WriteLine("Mensaje descifrado: " + mensajeDescifrado);
 
-            //Console.WriteLine("Clave Usada:");
-            //int clave = Convert.ToInt32(Console.ReadLine());
-
-            string mensaje = "Zizu patatas PaTatas";
-            int clave = 3;
-
-            string msgCifrado = encriptarCesar(mensaje, clave);
-            Console.WriteLine("Mensaje cifrado: " + msgCifrado);
-            
-
-            string mensajeDescifrado = desencriptarCesar(msgCifrado, clave);
-            Console.WriteLine("Mensaje descifrado: " + mensajeDescifrado);
-            Console.ReadKey();
-        }
-
-
-        private static string encriptarCesar(string mensaje, int clave)
-
-        {
-            
-
-            string resultado = "";
-
-            foreach (char c in mensaje) // Recorremos todos los ces del mensaje
-            {
-                if (char.IsLetter(c)) // Si es una letra
-                {
-                    char codificacion = Convert.ToChar(c + clave); 
-
-                    if ((char.IsLower(c) && codificacion > 'z') || (char.IsUpper(c) && codificacion > 'Z'))
-                    {
-                        resultado += Convert.ToChar(c - (26 - clave));
-                    }
-                    else
-                    {
-                        resultado += codificacion;
-                    }
-                }
-                else // No es una letra. (no hacer nada)
-                    resultado += c;
-
-            }
-
-            return resultado;
-        }
-
-        private static string desencriptarCesar(string mensajeCifrado, int clave)
-        {
-
-            string resultado = "";
-
-            foreach (char c in mensajeCifrado) // Recorremos todos los ces del mensaje
-            {
-                if (char.IsLetter(c)) // Si es una letra
-                {
-                    char codificacion = Convert.ToChar(c - clave);
-
-                    if ((char.IsLower(c) && codificacion < 'a') || (char.IsUpper(c) && codificacion < 'a'))
-                    {
-                        resultado += Convert.ToChar(c + (26 - clave));
-                    }
-                    else
-                    {
-                        resultado += codificacion;
-                    }
-                }
-                else // No es una letra. (no hacer nada)
-                    resultado += c;
-
-            }
-            return resultado;
-
-        }
-
-
-        private static bool letraOCaracter(char c)
-        {
-            return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
-        }
+        Console.ReadKey();
     }
 
+    private static string encriptarCesar(string mensaje, int clave)
 
+    {
+        string resultado = "";
+        int indice;
+        int nuevoIndice;
+        int intAscii;
+
+
+
+        foreach (char c in mensaje)
+        {
+            if (char.IsLetter(c))
+            {
+                if (char.IsUpper(c))
+                {
+                    indice = c - (int)'A' + clave;  // Índice de la letra en el alfabeto
+                    nuevoIndice = ((indice + clave) + 26) % 26;  // Nuevo índice después del desplazamiento, asegurándose de que esté dentro del rango [0, 25]
+                    intAscii = nuevoIndice + (int)'A';  // Convertir el nuevo índice a código ASCII
+                }
+                else if (char.IsLower(c))
+                {
+                    indice = c - (int)'a';  // Índice de la letra en el alfabeto
+                    nuevoIndice = ((indice + clave) + 26) % 26;  // Nuevo índice después del desplazamiento, asegurándose de que esté dentro del rango [0, 25]
+                    intAscii = nuevoIndice + (int)'a';  // Convertir el nuevo índice a código ASCII
+                }
+                else
+                {
+                    intAscii = c;  // Mantener caracteres que no son letras sin cambios
+                }
+          
+                resultado += (char)intAscii;
+            }
+            else
+            {
+                resultado += c;
+            }
+            Console.WriteLine("Resultado: " + (int)'A');
+        }
+
+        return resultado;
+    }
+
+    private static string desencriptarCesar(string mensajeCifrado, int clave)
+    {
+        return encriptarCesar(mensajeCifrado, -clave);
+    }
 }
